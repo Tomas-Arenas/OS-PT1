243777ff98a5df5f2e1083e9dcf235fc5796eaef
Tomas-Arenas <tomas.arenaz25@gmail.com>
2024-03-29 11:30:36 -0600

    test

--- findlocation.c
+++ findlocation.c
@@ -1,13 +1,34 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <stdio.h>
-#include <string.h>
-#include <errno.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/mman.h>
 
+static int binary_search(void *ptr, size_t size, char *word);
+static int binary_search_helper(nanpa_entry *dict, size_t n, char *word);
+
+
+typedef struct {
+  char prefix[6];    // 6 digits
+  char location[25]; // 25 characters
+  char newline;      // '\n'
+} nanpa_entry;
+
+static int my_atoi(const char *s) {
+    int result = 0;
+    int counter = 0;
+    while (s[counter] != '\0') {
+        if (s[counter] < '0' || s[counter] > '9') {
+            return -1;
+        }
+        result = result * 10 + s[counter] - '0';
+        counter++;
+    }
+    return result;
+}
+
+//my_write function
 static int my_write(int fd, const void *buf, size_t count) {
     size_t bytes_written = 0;
     size_t bytes_remaining = count;
@@ -24,6 +45,7 @@ static int my_write(int fd, const void *buf, size_t count) {
     }
     return 0;
 }
+
 static int my_strlen(const char *s) {
     int len = 0;
     while (*s != '\0') {
@@ -33,81 +55,70 @@ static int my_strlen(const char *s) {
     return len;
 }
 
-int my_print(const char *s) {
-    return my_write(1, s, my_strlen(s));
+
+static int binary_search(void *ptr, size_t size, char *word){
+    size_t n = size / sizeof(nanpa_entry);
+    if (size % sizeof(nanpa_entry) != 0) {
+        fprintf(stderr, "Size is not a multiple of nanpa_entry\n");
+        return -1;
+    }
+    return binary_search_helper((nanpa_entry *) ptr, n, word);
+
+}
+
+static int binary_search_helper(nanpa_entry *dict, size_t n, char *word)
+{
+    int low = 0;
+    int high = n - 1;
+    int mid;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (strcmp(dict[mid].prefix, word) == 0) {
+            my_write(1, "The location of ",  my_strlen("The location of "));
+            my_write(1, dict[mid].prefix, 6);
+            my_write(1, " is ", my_strlen(" is "));
+            my_write(1, dict[mid].location, 25);
+            my_write(1, "\n", my_strlen("\n"));
+            return 0;
+        } else if (strcmp(dict[mid].prefix, word) < 0) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return -1;
 }
 
 
-int main(int argc, char **argv) {
-    char *filename, *word;
+
+
+int main (int argc, char **argv) {
     int fd;
-    off_t res_lseek;
-    size_t file_size;
-    void *ptr;
-
-    if(argc < 2){/*Check for number of arguments*/
-         my_print("running without arguments\n");
-         return 1;
-    }
-    /* Make sure we have enough arguments */
-    if (argc < 3) {
-        fprintf(stderr, "Error: not enough arguments\n");
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <filename> <prefix>\n", argv[0]);
         return 1;
     }
-    filename = argv[1];
-    word = argv[2];
-
-    /* Open the file for reading */
-    fd = open(filename, O_RDONLY);
-    if (fd < 0) {
-        my_print("Error at opening the file\n");
+    fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        perror("open");
         return 1;
     }
-
-
-    res_lseek = lseek(fd,  0, SEEK_END);
-    if (res_lseek <  0) {
-        my_print("Error at using lseek\n");
+    off_t size = lseek(fd, 0, SEEK_END);
+    if (size == -1) {
+        perror("lseek");
         return 1;
     }
-
-    file_size = (size_t) res_lseek;
-
-    /* Handle the special case of a file that is empty */
-    if (file_size ==0 ) {
-        close(fd);
-        return 0;
-    }
-
-    /* Map the content of the file described by fd into memory */
-    ptr = mmap(NULL, file_size, PROT_READ, MAP_SHARED, fd, 0);
+    lseek(fd, 0, SEEK_SET);
+    void *ptr = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0);
     if (ptr == MAP_FAILED) {
-        my_print("Error at mapping the file to memory\n");
-        close(fd);
-        return 1;    
-    }
-
-
-    if (perform_lookup(ptr, file_size, word) < 0) {
-        my_print("Error at looking up in the file\n");
-        munmap(ptr,file-size);
-        close(fd);
+        perror("mmap");
         return 1;
     }
-
-    /* Unmap the memory */
-    if (munmap(ptr, file_size) < 0) {
-        my_print("Error at unmapping the file\n");
-        close(fd);
-        return 1;
+    int result = binary_search(ptr, size, argv[2]);
+    if (result == -1) {
+        fprintf(stderr, "Prefix not found\n");
     }
-
-    /* Close the file descriptor */
-    if (close(fd) < 0) {
-        my_print("Error at closing the file\n");
-        return 1;
-    }
-
-    /* Signal success */
+    munmap(ptr, size);
+    close(fd);
     return 0;
 }
